f1c48e203ef2b02c427e1424d67b9a8a2522c978
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2023-08-06 18:13:20 +0200

    feat: Acces Deta Base

---
+++ uploadToDataBase.py
@@ -0,0 +1,21 @@
+from deta import Deta
+from pathlib import Path
+
+# Initialize with a Project Key from DetaBaseKey.txt
+with open("DetaBaseKey.txt") as f:
+    key = f.read()
+
+deta = Deta(key)
+db = deta.Base("bricoheroes-base")
+
+# Upload all files from dataJSON/parsedData to the database
+filepath = Path("dataJSON/parsedData/")
+files = [f for f in filepath.iterdir() if f.is_file()]
+
+for file in files:
+    with open(file) as f:
+        fileContents = f.read()
+    print(fileContents)
+    print()
+    # db.put(fileContents, file.stem)
+    print(f"Uploaded {file.stem} to the database")
